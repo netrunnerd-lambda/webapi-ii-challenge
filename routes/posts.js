c4511932fe0130a024b90eb849b9e00fc687e7cc
netrunnerd <41193007+netrunnerd@users.noreply.github.com>
2019-07-26 23:00:32 -0400

    POST /api/posts, /api/posts/:id/comments

--- routes/posts.js
+++ routes/posts.js
@@ -33,4 +33,49 @@ router.get('/:id/comments', async (req, res) => {
   }
 });
 
+router.post('/', async (req, res) => {
+  const freshPost = req.body;
+  const { title, contents } = freshPost;
+
+  if (!title || !contents)
+    res.status(400).json({ success: false, message: "Please provide `title` and `contents` for post."}); 
+
+  try {
+    const posted = await db.insert(freshPost);
+
+    if (posted) {
+      const post = await db.findById(posted.id);
+      res.status(201).json({ success: true, post });
+    }
+  } catch (err) {
+    res.status(500).json({ success: false, message: "Post could not be saved." });
+  }
+});
+
+router.post('/:id/comments', async (req, res) => {
+  const { id } = req.params;
+  const { text } = req.body;
+
+  const post = await db.findById(id);
+
+  if (post.length === 0)
+    res.status(404).json({ success: false, message: "Post does not exist." });
+
+  if (!text)
+    res.status(400).json({ success: false, message: "Please provide `text` for comment." });
+
+  try {
+    const freshComment = { "post_id": id, text };
+    
+    const posted = await db.insertComment(freshComment);
+
+    if (posted) {
+      const comment = await db.findCommentById(posted.id);
+      res.status(201).json({ success: true, comment });
+    }
+  } catch (err) {
+    res.status(500).json({ success: false, message: "Comment could not be saved." });
+  }
+});
+
 module.exports = router;
